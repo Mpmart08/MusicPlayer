a4940d5947ff77efed9a09ee8256c91e95382025
Gerardo Prada <gpradag@msn.com>
2016-05-24 14:55:19 -0400

    Adds functionality to delete selected playlist from XML.

--- src/app/musicplayer/util/XMLEditor.java
+++ src/app/musicplayer/util/XMLEditor.java
@@ -211,13 +211,6 @@ public class XMLEditor {
 	}
 	
 	public static void deleteSongFromPlaylist(int selectedPlayListId, int selectedSongId) {
-		
-		// TODO: DEBUG
-		System.out.println("XMLE_216: selected playlist id = " + selectedPlayListId + 
-				" | selected playlist title = " + Library.getPlaylist(selectedPlayListId).getTitle() +
-				" | selected song id = " + selectedSongId + " | selected song title = " 
-				+ Library.getSong(selectedSongId).getTitle());
-		
         try {
 			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
@@ -233,6 +226,37 @@ public class XMLEditor {
             
             // Removes the node corresponding to the selected song.
             deleteSongNode.getParentNode().removeChild(deleteSongNode);
+                    
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(doc);
+            File xmlFile = new File(Resources.JAR + "library.xml");
+            StreamResult result = new StreamResult(xmlFile);
+            transformer.transform(source, result);
+            
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public static void deletePlaylistFromXML(int selectedPlayListId) {		
+        try {
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			Document doc = docBuilder.parse(Resources.JAR + "library.xml");
+			
+            XPathFactory xPathfactory = XPathFactory.newInstance();
+            XPath xpath = xPathfactory.newXPath();
+            
+            // Finds the node with the play list id for removal.
+            String query = "/library/playlists/playlist[@id='" + selectedPlayListId + "']";
+            XPathExpression expr = xpath.compile(query);
+            Node deleteplaylistNode = (Node) expr.evaluate(doc, XPathConstants.NODE);
+            
+            // Removes the node corresponding to the selected song.
+            deleteplaylistNode.getParentNode().removeChild(deleteplaylistNode);
                     
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
