f0cd54b0659bfe5cbed9f5cb8f0a45dcab4e2120
Gerardo Prada <gpradag@msn.com>
2016-05-26 01:58:59 -0400

    Creates menu for add to playlist button.

--- src/app/musicplayer/view/ControlPanelController.java
+++ src/app/musicplayer/view/ControlPanelController.java
@@ -4,10 +4,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import app.musicplayer.MusicPlayer;
+import app.musicplayer.model.Library;
+import app.musicplayer.model.Playlist;
 import app.musicplayer.util.SubView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 public class ControlPanelController implements Initializable {
@@ -16,9 +23,7 @@ public class ControlPanelController implements Initializable {
 	@FXML private Pane playlistButton;
 	
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-
-	}
+	public void initialize(URL location, ResourceBundle resources) {}
 	
 	@FXML
 	private void playSong(Event e) {
@@ -29,9 +34,34 @@ public class ControlPanelController implements Initializable {
 	
 	@FXML
 	private void addToPlaylist(Event e) {
-
+		// Gets the mouse event coordinates in the screen to display the context menu in this location.
+		MouseEvent mouseEvent = (MouseEvent) e;
+		double x = mouseEvent.getScreenX();
+		double y = mouseEvent.getScreenY();
+		
 		// TODO: DEBUG
-		System.out.println("CPC_34: add to playlist");
+		System.out.println("CPC_43: get x = " + mouseEvent.getScreenX() + " get y = " + mouseEvent.getScreenY());
+		
+		ObservableList<Playlist> playlists = Library.getPlaylists();
+		
+		// Retrieves all the playlist titles to create menu items.
+		ObservableList<String> playlistTitles = FXCollections.observableArrayList();
+		for (Playlist playlist : playlists) {
+			String title = playlist.getTitle();
+			if (!(title.equals("Most Played") || title.equals("Recently Played"))) {
+				playlistTitles.add(title);
+			}
+		}
+		
+		final ContextMenu contextMenu = new ContextMenu();
+		
+		// Creates a menu item for each playlist title and adds it to the context menu.
+		for (String title : playlistTitles) {
+			MenuItem item = new MenuItem(title);
+			contextMenu.getItems().add(item);
+		}
+		
+		contextMenu.show(playButton, x, y);
 		
 		e.consume();
 	}
